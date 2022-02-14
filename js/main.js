51ad43ce5ceb1d7402215a7d0db71fa50bd1a1b9
DXenium <48386878+DXenium@users.noreply.github.com>
2022-02-14 16:48:55 +0300

    Add files via upload
    Последняя домашка (дополненная)

--- js/main.js
+++ js/main.js
@@ -21,8 +21,23 @@ let menuMobile = document.querySelector('.header');
 
 burger.addEventListener('click', () => {
   menuMobile.style.display ='block';
-})
+});
 
 closeMenu.addEventListener('click', () => {
   menuMobile.style.display ='none';
-})
+});
+
+const modalWindow = document.querySelector('.modal');
+const buttonModal = document.querySelector('.main_button_play');
+
+buttonModal.addEventListener('click', (e) => {
+  modalWindow.classList.add('active');
+});
+
+modalWindow.addEventListener('click', (e) => {
+  const isModal = e.target.closest('.modal_inner');
+
+  if (!isModal){
+      modalWindow.classList.remove('active');
+  }
+});
