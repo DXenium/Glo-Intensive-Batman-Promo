09c5260d81962e080d69a7017d70bb9f6397d51d
DXenium <48386878+DXenium@users.noreply.github.com>
2022-02-14 13:35:09 +0300

    Add files via upload
    Последний 4й день интенсива

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 const swiper = new Swiper('.main_slider', {
-  slidesPerView: 2,
+  
   spaceBetween: 30,
   loop: true,
   navigation: {
     nextEl: '.main_slider_arrow',
   },
-
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    }
+  }
 });
+
+let burger = document.querySelector('.menu_burger');
+let closeMenu = document.querySelector('.header_menu_close');
+let menuMobile = document.querySelector('.header');
+
+burger.addEventListener('click', () => {
+  menuMobile.style.display ='block';
+})
+
+closeMenu.addEventListener('click', () => {
+  menuMobile.style.display ='none';
+})
